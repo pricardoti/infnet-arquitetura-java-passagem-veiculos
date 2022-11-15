7f20e1103d52ef05bbc3226a3000d974a8c16f37
Paulo Ricardo <pricardo.ti@gmail.com>
2022-11-15 01:55:13 -0300

    feat: reaproveitando os objetos criados na classe de teste do cliente

--- src/main/java/br/edu/infnet/passagemveiculos/controllers/ClienteController.java
+++ src/main/java/br/edu/infnet/passagemveiculos/controllers/ClienteController.java
@@ -5,32 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ClienteController {
 
+    private static List<Cliente> clientes = new ArrayList<>();
+
+    public static void incluir(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
     @GetMapping(value = "/cliente/lista")
     public String telaLista(Model model) {
-
-        Cliente cliente1 = new Cliente();
-        cliente1.setCpfCnpj("158.125.030-44");
-        cliente1.setNome("Primeiro Cliente");
-        cliente1.setEmail("nicifi3090@adroh.com");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setCpfCnpj("147.922.640-81");
-        cliente2.setNome("Segundo Cliente");
-        cliente2.setEmail("derrick.rath@gmail.com");
-
-        Cliente cliente3 = new Cliente();
-        cliente3.setCpfCnpj("28.197.869/0001-97");
-        cliente3.setNome("Empresa Cliente");
-        cliente3.setEmail("ettie73@gmail.com");
-
-        List<Cliente> clientes = List.of(cliente1, cliente2, cliente3);
         model.addAttribute("clientes", clientes);
-
         return "/cliente/lista";
     }
 }
